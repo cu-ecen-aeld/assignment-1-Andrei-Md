4e7c05429ee658486bc1bd76893098946013b74d
chh <chh@Ubuntu20.04.6.myguest.virtualbox.org>
2024-06-24 18:26:08 +0200

    - Added docstring  - changed ls with find command

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+###
+# Input:
+#  Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem,
+#    referred to below as filesdir; the second argument is a text string which will be searched within these files, referred to below as searchstr
+#  
+# Output:
+#  Exits with return value 1 error and print statements if any of the parameters above were not specified
+#  Exits with return value 1 error and print statements if filesdir does not represent a directory on the filesystem
+#
+#  Prints a message "The number of files are X and the number of matching lines are Y" 
+#   where X is the number of files in the directory and all subdirectories
+#         and Y is the number of matching lines found in respective files,
+#   where a matching line refers to a line which contains searchstr (and may also contain additional content).
+#
+###
+
 
 # Check the input parameters
 if [ "$#" -ne 2 ]; then
@@ -16,7 +32,7 @@ if [ ! -d $FILESDIR ]; then
     exit 1   
 fi
 
-FILESNO=$( ls $FILESDIR | wc -l )
+FILESNO=$( find $FILESDIR -type f | wc -l )
 MATCHINGLINES=$(grep -rnwl $FILESDIR -e $SEARCHSTR | wc -l)
 
 echo "The number of files are $FILESNO and the number of matching lines are $MATCHINGLINES"
